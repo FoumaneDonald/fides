1ac6048514eb990c86b1f3ef85255c8c6b96e546
FoumaneDonald <foumaned9@gmail.com>
2025-03-21 14:23:20 +0100

    first push

---
+++ android/app/src/main/kotlin/com/eos/fides/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.eos.fides
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../features/home_page.dart';
+
+class Routes {
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => HomePage(),
+      ),
+    ],
+    errorBuilder: (context, state) => Placeholder(),
+  );
+}
